f51a6f5a5e4f16ea76cab30845cd207df420dec8
Justin Mooney <justin.mooney.carmen@gmail.com>
2021-05-07 20:09:09 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header className="py-12 text-center">
+        <h1 className="text-4xl font-bold">Covid Tracker</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    screens: {
+      xs: '500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
